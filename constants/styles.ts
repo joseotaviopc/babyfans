68fe7e79efee492bd7ca18b5569518fe54635850
José Otavio Carvalho <carvalho.jose@gmail.com>
2023-12-20 08:40:14 -0300

    add assets & change styles

--- constants/styles.ts
+++ constants/styles.ts
@@ -1,4 +1,5 @@
 import { StyleSheet } from "react-native";
+import Colors from "./Colors";
 
 export const defaultStyles = StyleSheet.create({
   container: {
@@ -7,5 +8,44 @@ export const defaultStyles = StyleSheet.create({
   center: {
     justifyContent: 'center',
     alignItems: 'center',
-  }
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    height: 100,
+    width: '100%',
+    backgroundColor: Colors.dark.background,
+    borderTopRightRadius: 8,
+    borderTopLeftRadius: 8,
+    padding: 24,
+    borderTopEndRadius: 8,
+    borderTopStartRadius: 8,
+    // borderTopWidth: StyleSheet.hairlineWidth,
+    // borderTopColor: Colors.pink400
+    opacity: 0.9
+  },
+  inputField: {
+    height: 44,
+    borderWidth: 1,
+    borderColor: '#ABABAB',
+    borderRadius: 8,
+    padding: 10,
+    backgroundColor: '#fff',
+  },
+  btn: {
+    backgroundColor: Colors.pink500,
+    height: 50,
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btnText: {
+    color: '#fff',
+    fontSize: 16,
+    fontFamily: 'mon-b',
+  },
+  btnIcon: {
+    position: 'absolute',
+    left: 16,
+  },
 })
